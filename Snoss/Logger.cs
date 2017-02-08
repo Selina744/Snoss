62dab93a110221439913a06f9dd85b96222d24dd
Alexander <alhuertas@student.neumont.edu>
2017-02-08 08:33:46 -0700

    Made a logger class

--- Snoss/Logger.cs
+++ Snoss/Logger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,32 @@ namespace Snoss
 {
     class Logger
     {
-        
-        //takes in string and writes it to an output file
-        public Logger() { }
+
+
+        //Create a "logging level" for your OS.Log OS output to a file (or the console - your choice).  
+        //If the logging level is set high enough log "kernel" level process management info about which PID is executing and what instruction is being executed.
+        //Also, log when your OS decides to time slice and switch which process is in the CPU.
+
+        public LoggerLevels Level {get; private set;}
+        private StreamWriter outputFile;
+
+        public Logger(LoggerLevels level)
+        {
+            Level = level;
+        }
+
+        public void SetWriter(String outputName)
+        {
+            outputFile = new StreamWriter(@"./ " + outputName);
+        }
+
+        public void Log(String loggerData)
+        {
+            outputFile.Write(loggerData);
+        }
+
+
+
 
 
 
